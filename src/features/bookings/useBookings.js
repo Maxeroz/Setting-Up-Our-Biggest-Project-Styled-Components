c71e5606e13039f6e722ec9f159e41a66401da9f
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-08-05 22:55:56 +0400

    API-Side Sorting: Sorting Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -6,7 +6,6 @@ export function useBookings() {
   const [searchParams] = useSearchParams();
 
   // FILTER
-
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
@@ -14,13 +13,18 @@ export function useBookings() {
       : { field: "status", value: filterValue };
   // { field: "totalPrice", value: 5000, method: "gte" };
 
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryFn: () => getBookings({ filter }),
-    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy],
   });
 
   return { isLoading, bookings, error };
