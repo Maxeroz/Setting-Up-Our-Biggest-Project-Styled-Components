1e7dd8ff4fbc694aefc31a7e6ea9ec7fcb2cc938
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-08-09 22:34:13 +0400

    Adding Optional Breakfast

--- src/features/check-in-out/useCheckin.js
+++ src/features/check-in-out/useCheckin.js
@@ -12,8 +12,12 @@ export function useCheckin() {
     isLoading: isCheckingIn,
     error,
   } = useMutation({
-    mutationFn: (bookingId) =>
-      updateBooking(bookingId, { status: "checked-in", isPaid: true }),
+    mutationFn: ({ bookingId, breakfast }) =>
+      updateBooking(bookingId, {
+        status: "checked-in",
+        isPaid: true,
+        ...breakfast,
+      }),
 
     onSuccess: (data) => {
       toast.success(`Booking #${data.id} successfully checked in`);
