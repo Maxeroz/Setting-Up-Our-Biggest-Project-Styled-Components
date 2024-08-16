8c888285a6720c9bf7c371b9aab63aac69cb0a29
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-08-17 02:45:53 +0400

    User Sign Up

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,15 +3,17 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignUp";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { signup, isLoading } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
 
-  function onSubmit(data) {
-    console.log(data);
+  function onSubmit({ fullName, email, password }) {
+    signup({ fullName, email, password }, { onSettled: reset });
   }
 
   return (
@@ -19,6 +21,7 @@ function SignupForm() {
       <FormRow label="Full name" error={errors?.fullName?.message}>
         <Input
           type="text"
+          disabled={isLoading}
           id="fullName"
           {...register("fullName", { required: "This filed is required" })}
         />
@@ -27,6 +30,7 @@ function SignupForm() {
       <FormRow label="Email address" error={errors?.email?.message}>
         <Input
           type="email"
+          disabled={isLoading}
           id="email"
           {...register("email", {
             required: "This filed is required",
@@ -42,6 +46,7 @@ function SignupForm() {
       >
         <Input
           type="password"
+          disabled={isLoading}
           id="password"
           {...register("password", {
             required: "This filed is required",
@@ -56,6 +61,7 @@ function SignupForm() {
       <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
         <Input
           type="password"
+          disabled={isLoading}
           id="passwordConfirm"
           {...register("passwordConfirm", {
             required: "This filed is required",
@@ -67,10 +73,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
