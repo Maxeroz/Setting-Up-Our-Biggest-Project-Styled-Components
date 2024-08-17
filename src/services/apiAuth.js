de96ff7a5c29eecb669dc1cae05984e56c7997c6
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-08-18 01:17:13 +0400

    Authorization on Supabase: Protecting Database (RLS)

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,7 +1,13 @@
 import supabase from "./supabase";
 
 export async function signup({ fullName, email, password }) {
-  const { data, error } = await supabase.auth.signUp({
+  // Save the current session before signing up a new user
+  const { data: savedSessionData } = await supabase.auth.getSession();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.signUp({
     email,
     password,
     options: {
@@ -11,10 +17,32 @@ export async function signup({ fullName, email, password }) {
       },
     },
   });
+  // Log the entire response for debugging
+  console.log("Sign-up response:", { user, error });
 
-  if (error) throw new Error(error.message);
+  //If there was a previously authenticated user, restore their session
+  // This action should be placed right after signUp, otherwise the authError will stop the restore
+  if (savedSessionData) {
+    await supabase.auth.setSession(savedSessionData.session);
+  }
 
-  return data;
+  // Handle errors
+  let authError = null;
+  if (user && !user.identities.length) {
+    authError = {
+      name: "AuthApiError",
+      message: "This email has already been registered",
+    };
+  } else if (error) {
+    authError = {
+      name: error.name,
+      message: error.message,
+    };
+  }
+
+  if (authError) throw new Error(authError.message);
+
+  return user;
 }
 
 export async function login({ email, password }) {
